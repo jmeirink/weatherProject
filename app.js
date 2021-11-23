e24b4bd4e01e014696526be786712d50498fc2ed
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-11-23 08:16:42 -0600

    Integrated weather data into my server from openweathermap.org using a get request, and learned about retrieving status codes.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=3c65f1e5a7fe4d10131c5dd313bc3e12&units=metric";
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      console.log(data);
+    });
+  });
+
   res.send("Server is up and running");
 });
 
