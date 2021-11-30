a65eeee5df1fcad9dccfc71bef441e04f523363d
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-11-30 08:59:31 -0600

    Added a form for the user to input data and installed body parser into my project using npm

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.get("/", function(req, res){
-
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=3c65f1e5a7fe4d10131c5dd313bc3e12&units=metric";
-
-  https.get(url, function(response){
-    console.log(response.statusCode);
-
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      res.write("<p>The weather is currently " + weatherDescription + "</p>");
-      res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
-      res.send()
-    });
-  });
+  res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+  console.log("Post request recieved.")
+});
 
-
-
+  // const query = "London";
+  // const apiKey = "3c65f1e5a7fe4d10131c5dd313bc3e12";
+  // const unit = "metric";
+  // const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+  //
+  // https.get(url, function(response){
+  //   console.log(response.statusCode);
+  //
+  //   response.on("data", function(data){
+  //     const weatherData = JSON.parse(data);
+  //     const temp = weatherData.main.temp;
+  //     const weatherDescription = weatherData.weather[0].description;
+  //     const icon = weatherData.weather[0].icon;
+  //     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+  //     res.write("<p>The weather is currently " + weatherDescription + "</p>");
+  //     res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+  //     res.write("<img src=" + imageURL + ">");
+  //     res.send();
+  //   });
+  // });
 
 
 app.listen(3000, function(){
